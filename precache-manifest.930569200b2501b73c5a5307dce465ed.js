ef39c8fd3f9e813273ff3296e97897c32b5799a1
davaza <davaza5@gmail.com>
2021-01-15 18:36:26 +0500

    Add files via upload

---
+++ precache-manifest.930569200b2501b73c5a5307dce465ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "072d564ce8538cdab82a5537744956b9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "483701d1ea07d6210439",
+    "url": "./static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "1f4a317e395728e59f61",
+    "url": "./static/css/main.10dd1e3a.chunk.css"
+  },
+  {
+    "revision": "483701d1ea07d6210439",
+    "url": "./static/js/2.55db0f55.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.55db0f55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f4a317e395728e59f61",
+    "url": "./static/js/main.9e4c7bf1.chunk.js"
+  },
+  {
+    "revision": "b014c17175568a9a28f2",
+    "url": "./static/js/runtime-main.2e9003af.js"
+  }
+]);
